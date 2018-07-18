3f7a5eaee1814d17d429d272bd4c9f626508b411
Zac Willmington <zac.willmington@gmail.com>
2018-07-18 21:40:57 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,10 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
   def artist_name=(artist)
-      
+      binding.pry
   end
-    
-  def artist_name 
+
+  def artist_name
+      binding.pry
   end
 end
