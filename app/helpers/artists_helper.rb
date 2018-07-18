8215654b6f859abf89f88ff449e350bc571a4bfb
Zac Willmington <zac.willmington@gmail.com>
2018-07-18 22:05:52 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,6 +2,7 @@ module ArtistsHelper
 
     def display_artist(song)
         if song.artist == nil
+            binding.pry
              link_to "Add Artist", edit_song_path(song)
         else
             link_to song.artist.name, artist_path(song.artist)
