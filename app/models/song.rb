d7e89c196ea474157681f49a0b112d92b1cb72d6
Zac Willmington <zac.willmington@gmail.com>
2018-07-18 21:44:38 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,12 +2,12 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+      binding.pry
   end
 
   def artist_name=(name)
       binding.pry
       self.find_or_create_by(name: name, song: self)
       binding.pry
-      
   end
 end
